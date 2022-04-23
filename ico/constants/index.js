2a1f4b93049c09fa414aba3d6db4219d413138a8
Tushar <Tusharsolanki1011@gmail.com>
2022-04-23 14:00:08 +0530

    some changes

---
+++ ico/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS ="0xa62AC17B50DDEd30Fce1B18756F7cF9042A5BB8a"
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
